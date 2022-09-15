20c9b6a9b5b9989c59247e9da47d6668d7c99a15
gmogliani <112006025+gmogliani@users.noreply.github.com>
2022-09-15 16:32:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-// variables y funcion de orden superior
-const productos = [
-  { id: 1, nombre: "Pan de Masa Madre de Crema y Miel", precio: 100},
-  { id: 2, nombre: "Pan de Masa Madre Mix de semillas", precio: 200 },
-  { id: 3, nombre: "Miel batida x 500 gr", precio: 500 },
-]
-let nombre = parseInt(prompt("Ingrese el numero de producto para obtener mas informacion: \n\n1- Pan de Masa Madre de Crema y Miel \n\n2- Pan de Masa Madre Mix de semillas  \n\n3- Miel batida x 500 gr"));
-let panes = productos.find(item => item.id === nombre);
+// eventos
+function cambioProducto (){
+  let seleccion = document.getElementById("seleccion");
 
-if(panes === undefined){
-  alert("producto no encontrado");
-}else{
-  let mensaje = `
-id: ${panes.id}
-nombre: ${panes.nombre}
-precio: $${panes.precio}
-`;
-alert(mensaje);
+  if ("blanco" == seleccion.value){
+    document.getElementById("contenedor").innerHTML = '<img src="/img/pan_blanco.jpg" width="500px"><h1>Pan de Masa Madre de Crema y Miel</h1>\n<h2>Precio: $100</h2>';/*alert("Has seleccionado Pan Blanco de Crema y Miel\n\nPrecio: $100")*/
+    
+  }else if ("salvado" == seleccion.value){
+    document.getElementById("contenedor").innerHTML = '<img src="/img/pan_salvado.jpg" width="500px"><h1>Pan de Masa Madre de salvado</h1>\n<h2>Precio: $200</h2>';/*alert("Has seleccionado Pan de Salvado Mix de Semillas\n\nPrecio: $200")*/
+  }else{
+    contenedor.innerHTML = '<img src="https://i.postimg.cc/mg2305mQ/Pan-de-masa-madre-3-1024x1024-1.jpg" width="500">';
+  }
 }
